8c4b89cb91beaa508d7d961f166dcc6944b8e3c0
Miriam-Villalta <[email address]mvb_8@hotmail.com>
2021-05-24 22:27:36 +0200

    add favorite

--- src/js/02-fetch.js
+++ src/js/02-fetch.js
@@ -3,35 +3,39 @@ const filmList = document.querySelector(".js-films");
 let inputValue = document.querySelector(".js-search"); //input texto
 const searchBtn = document.querySelector(".js-btn"); //boton
 searchBtn.addEventListener("click", addListenerSearchClick);
+let dataFilms = [];
 
 function addListenerSearchClick() {
   inputValue = inputValue.value;
+  dataFilms = [];
   fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`)
     .then((response) => response.json())
     .then((data) => {
       console.log("Han llegado");
       console.log(data);
-
-      renderFilms(data);
+      data.forEach((element) => {
+        dataFilms.push(element.show);
+      });
+      renderFilms(dataFilms);
     });
 }
 
-function renderFilms(data) {
+function renderFilms(dataFilms) {
   //pintar elementos lista
   filmList.innerHTML = "";
   let newImage = "";
   let filmListContent = "";
-  for (let i = 0; i < data.length; i++) {
-    if (data[i].show.image === null) {
+  for (let i = 0; i < dataFilms.length; i++) {
+    if (dataFilms[i].image === null) {
       newImage = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
     } else {
-      newImage = data[i].show.image.medium;
+      newImage = dataFilms[i].image.medium;
     }
     filmListContent += `
-         <li class="films-each js-cards">
+         <li id ="${dataFilms[i].id}" class="films-each js-cards">
             <div class="films-each__container">
               <img src="${newImage}" alt="" class="films-each__container--photo">
-              <h3 class="films-each__container--text">${data[i].show.name}</h3>
+              <h3 class="films-each__container--text">${dataFilms[i].name}</h3>
             </div>
          </li>`;
   }
--- src/js/03-favorites.js
+++ src/js/03-favorites.js
@@ -11,17 +11,51 @@ function addListenersToCardsFilms() {
 
 function handlerFavorite(event) {
   const userClicked = event.currentTarget;
-  if (event) {
+  let idTarget = event.currentTarget.id;
+  const idSearch = newFav.find((element) => element.id === parseInt(idTarget));
+
+  if (idSearch == null) {
     userClicked.classList.add("select-fav"); //o toggle
     userClicked.classList.remove("films-each__container");
+    const idShows = dataFilms.find(
+      (element) => element.id === parseInt(idTarget)
+    );
+
+    newFav.push(idShows);
+  } else {
+    userClicked.classList.remove("select-fav");
+    userClicked.classList.add("films-each__container");
+    const idM = dataFilms.indexOf(idSearch);
+    newFav.splice(idM, 1);
   }
 
-  const newItem = newFav.push(renderFilms(data));
+  console.log(newFav);
 
-  console.log(newItem);
+  //pintar contenido en pelis favoritas
+  let newImage = "";
+  let filmListContent = "";
+  for (let i = 0; i < dataFilms.length; i++) {
+    if (dataFilms[i].image === null) {
+      newImage = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
+    } else {
+      newImage = dataFilms[i].image.medium;
+    }
+    filmListContent += `
+         <li id ="${dataFilms[i].id}" class="films-each js-cards">
+            <div class="films-each__container">
+              <img src="${newImage}" alt="" class="films-each__container--photo">
+              <h3 class="films-each__container--text">${dataFilms[i].name}</h3>
+            </div>
+         </li>`;
+  }
+
+  filmsFav.innerHTML = filmsFav + filmListContent; //es el que pinta el contenido
 }
 
-filmsFav.innerHTML = newItem;
+//1- meter solo una peli, no todo el listado
+//2- titulo pelis favoritas
+//3- localStorage
+//4- revisar busquedas
 
 /*if () {
   function paintFav() {
